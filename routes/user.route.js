346de9ed9de4d20e815c2c666990108751a60404
Moshood Alaran <geekyshood@gmail.com>
2020-03-27 19:54:49 +0100

    Added route for ask question and answer questions

--- routes/user.route.js
+++ routes/user.route.js
@@ -9,6 +9,9 @@ const Response = require("../services/response_manager");
 const httpStatus = require("../services/http_status");
 const Utils = require("../services/utilities");
 const User = require("../models/User.model");
+const Question = require("../models/Question.model");
+const { Answer } = require("../models/Answer.model");
+
 const { isAuthenticatedUser } = require("../middlewares/user.middleware");
 
 router.post("/register", (req, res) => {
@@ -161,6 +164,158 @@ router.post("/login", (req, res) => {
         httpStatus.FORBIDDEN
       );
     });
+});
+
+router.post("/ask", async (req, res) => {
+  try {
+    let errors = Utils.checkEmptyRequestBody(req.body, [
+      "userId",
+      "title",
+      "body"
+    ]);
+    if (Object.keys(errors).length !== 0) {
+      return Response.failure(
+        res,
+        {
+          message: "failure",
+          response: errors
+        },
+        httpStatus.BAD_REQUEST
+      );
+    }
+    let question = Question(req.body);
+    question = await question.save();
+    return Response.success(
+      res,
+      {
+        message: "success",
+        response: {
+          question: question
+        }
+      },
+      httpStatus.OK
+    );
+  } catch (error) {
+    if (error && error.code === 11000) {
+      return Response.failure(
+        res,
+        {
+          message: "failure",
+          response: {
+            question: "Duplicate entry, A similar question already asked"
+          }
+        },
+        httpStatus.INTERNAL_SERVER_ERROR
+      );
+    }
+    return Response.failure(
+      res,
+      {
+        message: "failure",
+        response: error
+      },
+      httpStatus.INTERNAL_SERVER_ERROR
+    );
+  }
+});
+router.post("/answer", async (req, res) => {
+  try {
+    let errors = Utils.checkAnswerRequestContent(req.body, [
+      "userId",
+      "questionId",
+      "body"
+    ]);
+    if (Object.keys(errors).length !== 0) {
+      return Response.failure(
+        res,
+        {
+          message: "failure",
+          response: errors
+        },
+        httpStatus.BAD_REQUEST
+      );
+    }
+    let question = await Question.findById(req.body.questionId);
+    let previousAnswers = question.answers;
+    if (!question) {
+      return Response.failure(
+        res,
+        {
+          message: "failure",
+          response: {
+            question: "Question not found"
+          }
+        },
+        httpStatus.BAD_REQUEST
+      );
+    }
+    let answer = new Answer(req.body);
+    let userAnsBeforeCount = 0;
+    previousAnswers.forEach((item, index) => {
+      if (item["userId"] == req.body.userId) {
+        userAnsBeforeCount += 1;
+      }
+    });
+    if (userAnsBeforeCount != 0) {
+      return Response.failure(
+        res,
+        {
+          message: "failure",
+          response: {
+            question:
+              "Duplicate entry, User already posted an answer on this question"
+          }
+        },
+        httpStatus.BAD_REQUEST
+      );
+    }
+    question.answers.push(answer);
+    question.save();
+    return Response.success(
+      res,
+      {
+        message: "success",
+        response: {
+          answer: answer
+        }
+      },
+      httpStatus.OK
+    );
+  } catch (error) {
+    return Response.failure(
+      res,
+      {
+        message: "failure",
+        response: error
+      },
+      httpStatus.INTERNAL_SERVER_ERROR
+    );
+  }
+});
+
+router.get("/questions", async (req, res) => {
+  try {
+    const questions = await Question.find();
+    return Response.success(
+      res,
+      {
+        message: "success",
+        response: {
+          question: questions
+        }
+      },
+      httpStatus.OK
+    );
+  } catch (error) {
+    return Response.failure(res, {
+      message: "failure",
+      response: error
+    });
+  }
+});
+
+router.get("/question", (req, res) => {
+  res.send("Route to view questions");
 });
 
 module.exports = router;
