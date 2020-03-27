1a71f7ed485a61639b9a5ec92a55497e3f74080b
Moshood Alaran <geekyshood@gmail.com>
2020-03-27 18:17:02 +0100

    Question model refactor

--- models/Question.model.js
+++ models/Question.model.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
 
 const questionSchema = mongoose.Schema({
+  title: {
+    type: String,
+    index: true,
+    unique: true
+  },
+  body: {
+    type: String,
+    default: null
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: null
+  },
   answers: {
     type: Array,
-    default: null
+    default: []
   },
   upvote: {
     type: Number,
@@ -13,16 +26,10 @@ const questionSchema = mongoose.Schema({
     type: Number,
     default: 0
   },
-  text: {
-    type: String,
-    index: true,
-    unique: true
-  },
-  authorId: {
-    type: mongoose.Schema.Types.ObjectId,
-    default: null
-  },
-  timestamp: Date.now()
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Question = new mongoose.model("Questions", questionSchema);
